359afc3084f31eece87ade86b88194259c615eb7
emilianoolbe <emilianoolbe@gmail.com>
2023-01-18 21:52:48 -0300

    !

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { trabajos } from '../data/trabajos'
 export const Portafolio = () => {
   return (
-    <div className='portafolio'>Portafolio</div>
+    <div className='portafolio'>
+      <h1>Trabajos</h1>
+      
+      {
+        trabajos.map((trabajo, i) => {
+          return (
+            <article key={i} className='portafolio-trabajo'>
+              <div className='mask'>
+                <img src={`/images/${trabajo.id}.png`} alt="" />
+              </div>
+              <h3><Link to={`/proyecto${trabajo.id}`}  >{trabajo.nombre}</Link></h3> <span>{trabajo.Categoria}</span>
+              <h3>Tecnologias: {trabajo.tecnologias}</h3>
+
+            </article>
+          )
+        })
+      }
+    </div>
   )
 }
---
+++ src/data/trabajos.js
@@ -0,0 +1,30 @@
+export const trabajos = [
+    {
+        'id':'mercadoliebre.uy',
+        'nombre': 'Mercado Liebre',
+        'url': 'mercadoliebre.uy',
+        'tecnologias': 'Front-End: CSS, EJS, Bootstrap, JS. Back-End: Node JS, Express, MySQL, Sequelize',
+        'Categoria' : 'Desarrollo Web'
+    },
+    {
+        'id':'rentATruck.uy',
+        'nombre': 'Rent A Trucl',
+        'url': 'rentATruck.uy',
+        'tecnologias': 'Front-End: CSS, EJS, Bootstrap, JS. Back-End: Node JS, Express, MySQL, Sequelize',
+        'Categoria' : 'Desarrollo Web'
+    },
+    {
+        'id':'dashboardRenAtruck',
+        'nombre': 'Rent A Truck - dashboard',
+        'url': 'rentATruck.dashboard.uy',
+        'tecnologias': 'React JS, JS, CSS, JSX',
+        'Categoria' : 'Desarrollo de API REST'
+    },
+    {
+        'id':'reactMovies',
+        'nombre': 'Movies',
+        'url': 'movies.uy',
+        'tecnologias': 'React JS, JS, SCSS, GRID, FLEXBOX, JSX',
+        'Categoria' : 'Desarrollo web en React'
+    }
+];
